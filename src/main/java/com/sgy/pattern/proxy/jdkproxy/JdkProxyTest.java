4f5d8ba51a49aa64b72969e8c5a9a41dcf386433
sgy <sgy0513@163.com>
2017-11-13 11:26:05 +0800

    提交

--- src/main/java/com/sgy/pattern/proxy/jdkproxy/JdkProxyTest.java
+++ src/main/java/com/sgy/pattern/proxy/jdkproxy/JdkProxyTest.java
@@ -15,7 +15,6 @@ public class JdkProxyTest {
 	public static void main(String[] args) {
 		//获取被代理人个人信息
 		Person obj = (Person) new YueLao().getInstance(new Tony());
-		
 		obj.findLove();
 	}
 	//原理：
